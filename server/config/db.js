dcc52a783e0ccfa04dd6234bf7e49f7dc9840820
meet2829 <meetpatel93160@gmail.com>
2025-07-01 13:40:27 +0530

    Initial backend commit

--- server/config/db.js
+++ server/config/db.js
@@ -4,8 +4,7 @@ require("dotenv").config(); // Load environment variables
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-       useNewUrlParser: true,
-  useUnifiedTopology: true
+      
     });
     console.log("MongoDB connected ✅");
   } catch (err) {
