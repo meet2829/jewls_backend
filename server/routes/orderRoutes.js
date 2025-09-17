05aa6c0c0e329d5d5bc06e75f197c36d95b90cf3
meet2829 <meetpatel93160@gmail.com>
2025-09-17 18:12:03 +0530

    .

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controller/orderController");
-const { getOrders, getOrderDetails } = require("../controllers/orderController");
+const { getOrders, getOrderDetails } = require("../controller/orderController");
+
 router.get("/", getOrders); // GET all orders
 router.get("/:id", getOrderDetails); // GET single order details
 
